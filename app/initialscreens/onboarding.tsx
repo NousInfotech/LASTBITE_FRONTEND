9fa346afe1d7b1e3bd774141ed87cbb7cbd69c43
yaxshapaul5 <yaxshapaull@gmail.com>
2025-02-19 19:05:01 +0530

    Initail Page

--- app/initialscreens/onboarding.tsx
+++ app/initialscreens/onboarding.tsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { View, Text, SafeAreaView, Animated, Platform } from "react-native";
 import { Image } from "expo-image";
 import { useNavigation, useRouter } from "expo-router";
+import * as Font from "expo-font";
 import Button from "@/components/ButtoN";
 
 const Onboarding = () => {
   const navigation = useNavigation();
   const [activeOption, setActiveOption] = useState<string>("Food");
   const [fadeAnim] = useState(new Animated.Value(1)); 
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   const options: string[] = ["Food", "Instamart", "Dineout"];
 
   const images: Record<string, any> = {
@@ -15,6 +17,19 @@ const Onboarding = () => {
     Instamart: require("../../assets/images/supermart.gif"),
     Dineout: require("../../assets/images/OrderTracking.gif"),
   };
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({
@@ -25,19 +40,18 @@ const Onboarding = () => {
       handleOptionChange(
         options[(options.indexOf(activeOption) + 1) % options.length]
       );
-    }, 2000); 
+    }, 2000);
 
-    return () => clearInterval(interval); 
+    return () => clearInterval(interval);
   }, [activeOption]);
 
   const handleOptionChange = (option: string) => {
-    // Fade out the image
     Animated.timing(fadeAnim, {
       toValue: 0,
       duration: 200,
       useNativeDriver: true,
     }).start(() => {
-      setActiveOption(option); 
+      setActiveOption(option);
       Animated.timing(fadeAnim, {
         toValue: 1,
         duration: 200,
@@ -47,6 +61,11 @@ const Onboarding = () => {
   };
 
   const router = useRouter();
+
+  if (!fontsLoaded) {
+    return null; 
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <View
@@ -68,16 +87,14 @@ const Onboarding = () => {
         >
           <Animated.View style={{ opacity: fadeAnim }}>
             <Image
-              source={images[activeOption]} 
+              source={images[activeOption]}
               style={{ width: 400, height: 400, resizeMode: "contain" }}
             />
           </Animated.View>
         </View>
 
         <View style={{ alignItems: "center", marginBottom: 32 }}>
-          <View
-            style={{ flexDirection: "row", marginTop: 32, marginBottom: 8 }}
-          >
+          <View style={{ flexDirection: "row", marginTop: 32, marginBottom: 8 }}>
             <Image
               source={require("../../assets/images/logo2.png")}
               style={{ width: 300, height: 50, resizeMode: "contain" }}
@@ -88,8 +105,8 @@ const Onboarding = () => {
             style={{
               color: "#929292",
               textAlign: "center",
-              fontSize: 16,
-              fontFamily: "myCustomFont",
+              fontSize: 14,
+              fontFamily: "Poppins-Regular",
             }}
           >
             Enjoy meals from popular spots around you
@@ -105,14 +122,11 @@ const Onboarding = () => {
           }}
         >
           {options.map((option, index) => (
-            <View
-              key={option}
-              style={{ flexDirection: "row", alignItems: "center" }}
-            >
+            <View key={option} style={{ flexDirection: "row", alignItems: "center" }}>
               <Text
                 style={{
-                  fontFamily: "myCustomFont",
-                  fontWeight: activeOption === option ? "600" : "normal",
+                  fontFamily: "Poppins-Medium",
+                  fontWeight: activeOption === option ? "400" : "normal",
                   color: activeOption === option ? "black" : "#4a4a4a",
                   fontSize: Platform.OS === "ios" ? 20 : 20,
                 }}
